2992a1f1437ae2e96c710d909d8e81679e7be62a
zira4ka <irashemchuk7@gmail.com>
2016-04-13 22:13:04 +0300

    class point

---
+++ 111/111/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+
+            Console.ReadLine();
+
+        }
+    }
+}
